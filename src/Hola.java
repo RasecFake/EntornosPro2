4d8678b84810f5e607f854ff4a7b5e7053c5d7d6
cesar <ctornav0706@g.educaand.es>
2026-01-16 10:05:32 +0100

    cambios en casa

--- src/Hola.java
+++ src/Hola.java
@@ -1,5 +1,7 @@
 public class Hola {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("uwu");
+        System.out.println("siseñorencasa");
     }
 }
